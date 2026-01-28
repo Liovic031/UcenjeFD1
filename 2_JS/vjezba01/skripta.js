87fb909d0c5507583ee1fa0ab2cd22d3077a837d
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-01-28 14:16:26 +0100

    JavaScript uvod Hello World

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,12 @@
+console.info('Hello world 5');
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'));
+
+console.log('O\'Neal'); // \ ALTGR + Q
+console.log("O'Neal");
+
+console.log(`
+Hello
+World
+6`) // ` ALTGR + 7
+
+document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
