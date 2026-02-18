1fb30e243ee5ca25eb5349b6f5753c37b8bdcd54
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-02-19 00:31:57 +0100

    Generator Lozinki pt2

--- 2_JS/vjezba07/generator/skripta.js
+++ 2_JS/vjezba07/generator/skripta.js
@@ -13,7 +13,7 @@ document.getElementById('izvedi').addEventListener('click', () => {
     const zavrsetakBroj = document.getElementById('zavrsetakBroj').checked;
     const zavrsetakZnak = document.getElementById('zavrsetakZnak').checked;
 
-    const ponavljanje = document.getElementById('ponavljanje').checked;
+    const nePonavljanje = document.getElementById('nePonavljanje').checked;
 
     // SVI ZNAKOVI
     const vs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
@@ -37,12 +37,12 @@ document.getElementById('izvedi').addEventListener('click', () => {
     }
 
     // SIGURNOSNE PROVJERE
-    if(isNaN(duzinaLozinke) || duzinaLozinke < 1){ 
-        rezultat.innerHTML = "Unesite ispravnu dužinu lozinke."; 
+    if(isNaN(duzinaLozinke) || duzinaLozinke < 3 || duzinaLozinke > 25){ 
+        rezultat.innerHTML = "Unesite dužinu lozinke između 3 i 25."; 
         return; 
     }
     if(isNaN(brojLozinki) || brojLozinki < 1){ 
-        rezultat.innerHTML = "Unesite ispravan broj lozinki."; 
+        rezultat.innerHTML = "Unesite broj lozinki."; 
         return; 
     }
     if(dozvoljeniSimboli.length === 0){ 
@@ -57,38 +57,131 @@ document.getElementById('izvedi').addEventListener('click', () => {
         rezultat.innerHTML = "Lozinka ne može završavati s brojem i sa znakom";
         return; 
     }
+    if(nePonavljanje && pocetakBroj && !brojevi) {
+        rezultat.innerHTML = "Lozinka treba početi brojem, ali brojevi nisu uključeni.";
+        return;
+    }
+    if(nePonavljanje && pocetakZnak && !interpunkcijskiZnakovi) {
+        rezultat.innerHTML = "Lozinka treba početi interpukcijskim znakom, ali interpukcijski znakovi nisu uključeni.";
+        return;
+    }
+    if (nePonavljanje && zavrsetakBroj && !brojevi) {
+        rezultat.innerHTML = "Lozinka treba završiti brojem, ali brojevi nisu uključeni.";
+        return;
+    }
+    if (nePonavljanje && zavrsetakZnak && !interpunkcijskiZnakovi) {
+        rezultat.innerHTML = "Lozinka treba završiti znakom, ali znakovi nisu uključeni.";
+        return;
+    }
+    if (nePonavljanje && duzinaLozinke > dozvoljeniSimboli.length) {
+        rezultat.innerHTML = "Nema dovoljno različitih znakova za lozinku bez ponavljanja. Smanjite dužinu lozinke ili omogućite još znakova!";
+        return;
+    }
 
+    
     // ALGORITAM
     let sveLozinke = "";
-    
+
     for(let i = 0; i < brojLozinki; i++){
-        // PRVI ZNAK
+
+        // I. PRVI ZNAK
+
         let lozinka = ''
-        if(pocetakBroj){
-            lozinka += br[Math.floor(Math.random() * br.length)];
+        // varijable koje se koriste u slučaju da se znakovi ne smiju ponavljati
+        let lokalniSimboli = dozvoljeniSimboli;
+        let lokalniBr = br;
+        let lokalniZk = zk;
+        let zadnjiZnak = '';
+        //
+        if(pocetakBroj){ // ukoliko je prvi znak broj
+
+            if(nePonavljanje){ //Samo u slučaju da se znakovi ne smiju ponavljati
+                lozinka += lokalniBr[Math.floor(Math.random() * lokalniBr.length)];
+                zadnjiZnak = lozinka[lozinka.length-1];
+                lokalniSimboli = lokalniSimboli.replace(zadnjiZnak,'');
+                lokalniBr = lokalniBr.replace(zadnjiZnak,'');
+            }
+            else{ // Ako se znakovi smiju ponavljati
+                lozinka += br[Math.floor(Math.random() * br.length)];
+            }
+
         }
-        else if(pocetakZnak){
-            lozinka += zk[Math.floor(Math.random() * zk.length)];
+        else if(pocetakZnak){ // Ukoliko je prvi znak interpukcijski znak
+
+            if(nePonavljanje){ // Isti način
+                lozinka += lokalniZk[Math.floor(Math.random() * lokalniZk.length)];
+                zadnjiZnak = lozinka[lozinka.length-1];
+                lokalniSimboli = lokalniSimboli.replace(zadnjiZnak,'');
+                lokalniZk = lokalniZk.replace(zadnjiZnak,'');
+            }
+            else{
+                lozinka += zk[Math.floor(Math.random() * zk.length)];
+            }
+
         }
-        else{
-            lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+        else{ // ukoliko je svejedno koji je prvi znak
+
+            if(nePonavljanje){ // Isti način
+                lozinka += lokalniSimboli[Math.floor(Math.random() * lokalniSimboli.length)];
+                zadnjiZnak = lozinka[lozinka.length-1];
+                lokalniSimboli = lokalniSimboli.replace(zadnjiZnak,''); // izbacivanje simbola iz stringa
+                lokalniZk = lokalniZk.replace(zadnjiZnak,''); // zanemarit će se ako se simbol ne nalazi u stringu
+                lokalniBr = lokalniBr.replace(zadnjiZnak,''); // zanemarit će se ako se simbol ne nalazi u stringu
+            }
+            else{
+                lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+            }
         }
-        // SREDINA
+
+
+        // II. SREDINA
+
         for (let j = 1; j < duzinaLozinke - 1; j++){
-            lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+            if(nePonavljanje){ // Isti način kao za prvi znak
+                lozinka += lokalniSimboli[Math.floor(Math.random() * lokalniSimboli.length)];
+                zadnjiZnak = lozinka[lozinka.length-1];
+                lokalniSimboli = lokalniSimboli.replace(zadnjiZnak,'');
+                lokalniZk = lokalniZk.replace(zadnjiZnak,'');
+                lokalniBr = lokalniBr.replace(zadnjiZnak,'');
+            }
+            else{
+                lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+            }
         }
-        // ZADNJI ZNAK
+
+
+        // III. ZADNJI ZNAK
+
         if(zavrsetakBroj){
-            lozinka += br[Math.floor(Math.random() * br.length)];
+            if(nePonavljanje){
+                lozinka += lokalniBr[Math.floor(Math.random() * lokalniBr.length)];
+                // ne treba izbacivati znakova jer je ovo zadnji znak lozinke
+            }
+            else{
+                lozinka += br[Math.floor(Math.random() * br.length)];
+            }        
         }
+
         else if(zavrsetakZnak){
-            lozinka += zk[Math.floor(Math.random() * zk.length)];
+            if(nePonavljanje){
+                lozinka += lokalniZk[Math.floor(Math.random() * lokalniZk.length)];
+            }
+            else{
+                lozinka += zk[Math.floor(Math.random() * zk.length)];
+            }
         }
+
         else{
-            lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+            if(nePonavljanje){
+                lozinka += lokalniSimboli[Math.floor(Math.random() * lokalniSimboli.length)];
+            }
+            else{
+                lozinka += dozvoljeniSimboli[Math.floor(Math.random() * dozvoljeniSimboli.length)];
+            }
         }
+
         // SPREMANJE LOZINKE U STRING SVIH LOZINKI
-        sveLozinke += lozinka + ' , ';
+        sveLozinke += lozinka + '<br>';
     }
 
     rezultat.innerHTML = sveLozinke;
