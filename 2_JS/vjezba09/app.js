62c987108a8c4ddcb6c78e87d4ef5806347ead27
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-02-19 20:59:59 +0100

    vjezba09

---
+++ 2_JS/vjezba09/app.js
@@ -0,0 +1,105 @@
+
+const isDev = true;
+
+// funkcija je skup naredbi s određenom svrhom koji ćemo više puta pozivati => DRY (don't repeat yourself)
+
+// osnovne vrste funkcija
+
+// 1. ne prima vrijednost, ne vraća vrijednost
+// poziv funkcije koji je u kodu prije definicije funkcije
+pozdraviSvijet();
+// definiranje funkcije
+function pozdraviSvijet(){ // ona je tipa void
+    console.log('Hello world');
+    // return; može i ne mora se napisati
+}
+// poziv funkcije
+pozdraviSvijet();
+
+// 2. prima parametre, ne vraća vrijednost
+// definiranje funkcije
+function parniBrojevi(odBroja, doBroja){ // parametri se odvajaju zarezom, u ovom primjeru su varijable
+    const max = odBroja > doBroja ? odBroja : doBroja;
+    const min = odBroja < doBroja ? odBroja : doBroja;
+    for(let i = min; i <= max; i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+}   
+//poziv funkcije
+log('Prvi poziv');
+parniBrojevi(0, 8);
+log('Drugi poziv');
+parniBrojevi(59, 65);
+log('Treći poziv');
+parniBrojevi(12, 7);
+
+function log(poruka){
+    if(!isDev){
+        return;
+    }
+    console.log('-----------------------');
+    console.log(poruka);
+    console.log('-----------------------');
+}
+
+// 3. ne prima parametre, vraća vrijednost
+// definiranje funkcije
+function slucajniBroj(){ // ona je tipa number
+    const broj = Math.random();
+    log(broj);
+    const uvecano = broj * 1000;
+    const intb = parseInt(uvecano);
+    return intb; // ovo je vraćanje vrijednosti
+}
+// i funkciju koja vraća vrijednost možemo pozvati bez da ju dodjelimo nekome/nečemu
+slucajniBroj();
+// ispravan način poziva
+const sb = slucajniBroj();
+log(sb);
+
+// slučajni broj načini
+console.log((Math.random()*1000).toFixed(0)); //slučajni od 100 do 999
+console.log((Math.random() * (58-22) + 22).toFixed(0)); // od 22 do 58
+
+
+// 4. prima parametre, vraća vrijednost ---> najčešće korišten
+// definiranje funkcije
+function zbrojPrimBrojeva(odBroja, doBroja){
+    let suma = 0, prim = true;
+    for(let i = odBroja; i <= doBroja; i++){
+        prim = true;
+        for(let j = 2; j < i; j++){
+            if(i % j === 0){
+                prim = false;
+                break;
+            }
+        }
+        if(prim){
+            suma += i;
+        }
+    }
+    return suma;
+}
+
+log(zbrojPrimBrojeva(3,80));
+
+// pozdravArrow(); ne može ju se pozvati prije inicijalizacije
+
+// arrow funkcija
+const pozdravArrow = () => console.log('Hello Arrow'); // vjerovali ili ne ovo je funkcija
+
+pozdravArrow();
+
+// funkcija u objektu
+
+const brojevi = {
+    slucajniBroj: () => {
+        // tu bi došao kod koji imam u funkciji slucajniBroj
+        return 7;
+    },
+    nula: () => {return 0;}
+}
+
+log(brojevi['slucajniBroj']());
