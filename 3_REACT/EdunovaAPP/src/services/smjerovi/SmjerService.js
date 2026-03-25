92c6b94504efbe78b72761cceceb86eb511942be
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-03-25 18:53:11 +0100

    Priprema promjene smjera

--- 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerService.js
+++ 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerService.js
@@ -5,6 +5,10 @@ async function get() {
     return {data: smjerovi}
 }
 
+async function getBySifra(sifra) {
+    return {data: smjerovi.find(s => s.sifra === parseInt(sifra))}
+}
+
 // 2/4 Create of CRUD
 async function dodaj(smjer){
     smjer.sifra = smjerovi.length > 0 ? smjer.sifra = smjerovi[smjerovi.length - 1].sifra + 1 : smjer.sifra = 1;
@@ -12,9 +16,22 @@ async function dodaj(smjer){
     smjerovi.push(smjer);
 }
 
+// 3/4 Update of CRUD
+async function promjeni(sifra, smjer) {
+    const index = nadiIndex(sifra)
+    smjerovi[index] = {...smjerovi[index], ...smjer}
+}
+
+function nadiIndex(sifra){
+    return smjerovi.findIndex(s => s.sifra === parseInt(sifra))
+}
+
+
 
 export default{
     get,
-    dodaj
+    dodaj,
+    getBySifra,
+    promjeni
 }
 
